9c3103c3e9a0cbf69485672ce302cdab0d8d22c5
Fehmi Günay <82449304+fehmig@users.noreply.github.com>
2023-04-03 17:21:34 +0300

    login

--- src/Components/login.js
+++ src/Components/login.js
@@ -17,72 +17,47 @@ const Login = () => {
         <>
 
             <Navbar />
-            {/* <br /> <br /> <br /> <br /> <br /> */}
+            <br/> <br/> <br/> <br/> <br/>
+           
             <div className="routepage">
-                {/* <i><h1>GİRİŞ SAYFASI</h1>  </i> */}
-                {/* <br /> <br /> <br /> <br /> <br /> */}
-                {/* deneme */}
-                <section id='home' className='home'>
-                    <div className="overlay"></div>
-                    <video src={video} autoPlay loop muted type="video/mp4"></video>
-
-                    <div data-aos="fade-down" className="homeContent container">
-                        <div className="textDiv">
-                            <span className="smallText">
-                                LOGİN 
-                            </span>
-                            {/* <h1 data-aos="fade-down" className="homeTitle">
-                                Search your Travel routes!
-                            </h1> */}
-
-<>
-            <div className="sonucv2">
-             
-                <form action="POST">
-                    <input
-                    type='text'
-                    placeholder="Kullanıcı Adı"
-               
-                    
-                    />
-                    <br/> <br/>
-                    <input
-                    type='password'
-                    placeholder="Şifre"
-                   
-                    
-                    />
-                    <br/>  <br/>
-                    <button type="submit" className="btn-yorum">GİRİŞ</button>
-                </form>
-                
-                    <br/>  <br/>
-                <div>
-                  
+                <div className="video-background-login">
+                  <video src={video} autoPlay loop muted type="video/mp4"></video>
                 </div>
+                <div className="sonucv2">
+                <h1>Turmep LOGIN </h1>
+                    <form action="POST">
+                        <input className="input-login"
+                            type='text'
+                            placeholder="Username"
+
+
+                        />
+                        <br /> <br />
+                        <input className="input-login"
+                            type='password'
+                            placeholder="Password"
+
+
+                        />
+                        <br />  <br />
+                        <button type="submit" className="btn-yorum">LOGIN</button>
+                    </form>
+
+            
+                </div>
+
+                    
+
+
             </div>
+
 
            
-            </>
-                        </div>
-                    </div>
-                </section>
-
-                <div>
-
-                </div>
 
 
 
 
 
-                {/* deneme */}
-                <div className="login">
-                    <h3> </h3>
-
-                </div>
-            </div>
-            
 
         </>
 
