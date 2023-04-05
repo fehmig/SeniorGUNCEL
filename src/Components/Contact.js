3ec1982fa0abec316c812a1f0d7630b675411075
Fehmi Günay <82449304+fehmig@users.noreply.github.com>
2023-04-05 17:45:55 +0300

    ..

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,7 +3,7 @@ import Navbar from "./Navbar/Navbar";
 import Home from "./Home/Home";
 import Footer from "./Footer/Footer";
 
-import {FiSend} from 'react-icons/fi'
+import { FiSend } from 'react-icons/fi'
 import contactimage from '../Assets/contact.png'
 
 const Contact = () => {
@@ -16,61 +16,61 @@ const Contact = () => {
             <Navbar />
             <br /> <br /> <br />
             <div className="contact-page">
-            <div className="contact-sol">
-                <div className="contact">
-                    <div><h1 style={{color:"hsl(199, 100%, 33%)",textShadow: '1px 1px 0 hsl(200 50% 30%)'}}>!CONTACT US</h1></div>
-                    <br />
-                    <input
-                        className='input-contact'
-                        type="text"
-                        placeholder='Name'
+                <div className="contact-sol">
+                    <div className="contact">
+                        <div><h1 style={{ color: "hsl(199, 100%, 33%)", textShadow: '1px 1px 0 hsl(200 50% 30%)' }}>!CONTACT US</h1></div>
+                        <br />
+                        <input
+                            className='input-contact'
+                            type="text"
+                            placeholder='Name'
 
 
-                    />
-                    <br /><br />
+                        />
+                        <br /><br />
 
-                    <input
-                        className='input-contact'
-                        type="text"
-                        placeholder='Surname'
-
-
-                    />
-                    <br /><br />
-
-                    <input
-                        className='input-contact'
-                        type="email"
-                        placeholder='E-Mail'
+                        <input
+                            className='input-contact'
+                            type="text"
+                            placeholder='Surname'
 
 
-                    />
-                    <br /><br />
-                   
-                    <textarea name="message" rows="4" cols="50"
+                        />
+                        <br /><br />
 
-                        className="text-contact"
-                        type='text'
-                        placeholder="Message here..."
-                       
-                    ></textarea>
-   <br />
-<button  className="btn-yorum">SEND</button>
+                        <input
+                            className='input-contact'
+                            type="email"
+                            placeholder='E-Mail'
 
 
-                </div>
+                        />
+                        <br /><br />
+
+                        <textarea name="message" rows="4" cols="50"
+
+                            className="text-contact"
+                            type='text'
+                            placeholder="Message here..."
+
+                        ></textarea>
+                        <br />
+                        <button className="btn-yorum">SEND</button>
+
+
+                    </div>
                 </div>
 
                 <div className="contact-sag">
                     <div className="contact-adres">
                         <div>
-                        <b>ADRES:</b> Karabük Üniversitesi <br/> Demir-Çelik Kampüsü Mühendislik Fakültesi Balıklarkayası Mevkii <br/>
-                         78050  KARABÜK
+                            <b>ADRES:</b> Karabük Üniversitesi <br /> Demir-Çelik Kampüsü Mühendislik Fakültesi Balıklarkayası Mevkii <br />
+                            78050  KARABÜK
                         </div>
-                        <br/> <br/>
+                        <br /> <br />
                         <div className="contact-image">
-                            <img src={contactimage} style={{width:"600px", height:"400px"}}></img>
-                   
+                            <img src={contactimage} style={{ width: "600px", height: "400px" }}></img>
+
                         </div>
                     </div>
                 </div>
--- src/Components/Forum.js
+++ src/Components/Forum.js
@@ -14,16 +14,16 @@ const Forum = () => {
             <>
 
                 <Navbar />
-                <br /> <br /> <br />
-                <div className="about-page">
+                <br /> <br /> <br /><br /><br />
+                <div className="forum-page">
                     <i><h1>FORUM SAYFASI</h1>  </i>
 
 
 
 
-                    <br /> <br />
+                    <br />
 
-                    <div className="about">
+                    <div className="forum">
 
                         <hr></hr>
                         <br></br>
@@ -34,100 +34,99 @@ const Forum = () => {
                         <br></br>
 
                         <hr></hr>
-                        <svg height="100" width="100">
+                        {/* <svg height="100" width="100">
                             <line x1="0" y1="50" x2="100" y2="50" stroke="black" stroke-width="10" />
-                        </svg>
+                        </svg> */}
 
 
                     </div>
+                    <div className="forum-alt">
+                        <div className="forum-sol">
+                            <div className="forum">
+                                <div><h1 style={{ color: "hsl(199, 100%, 33%)", textShadow: '1px 1px 0 hsl(200 50% 30%)' }}>KONU BAŞLIĞI AÇ!</h1></div>
+                                <br />
+                                <div className="forum-solalt-sol">
+                                    <br></br>
+                                    <br></br>
+
+                                    <input
+                                        className='input-forum'
+                                        type="text"
+                                        placeholder='Name'
 
 
-                </div>
-                <div className="forum-sol">
-                    <div className="forum">
-                        <div><h1 style={{ color: "hsl(199, 100%, 33%)", textShadow: '1px 1px 0 hsl(200 50% 30%)' }}>!KONU BAŞLIĞI AÇ</h1></div>
-                        <br />
-                        <div className="forum-solalt-sol">
-                            <br></br>
-                            <br></br>
+                                    />
+                                    <br /><br />
 
-                            <input
-                                className='input-forum'
-                                type="text"
-                                placeholder='Name'
+                                    <input
+                                        className='input-forum'
+                                        type="text"
+                                        placeholder='Surname'
 
 
-                            />
-                            <br /><br />
+                                    />
+                                    <br /><br />
 
-                            <input
-                                className='input-forum'
-                                type="text"
-                                placeholder='Surname'
-
-
-                            />
-                            <br /><br />
-
-                            <input
-                                className='input-forum'
-                                type="email"
-                                placeholder='E-Mail'
+                                    <input
+                                        className='input-forum'
+                                        type="email"
+                                        placeholder='E-Mail'
 
 
-                            />
+                                    />
+                                </div>
+                                <div className="forum-solalt-sağ">
+                                    <br /><br />
+
+                                    <textarea name="message" rows="4" cols="50"
+
+                                        className="text-forum"
+                                        type='text'
+                                        placeholder="Message here..."
+
+                                    ></textarea>
+                                    <br />
+                                    <button className="btn-yorum">SEND</button>
+
+                                </div>
+
+
+
+
+                            </div>
                         </div>
-                        <div className="forum-solalt-sağ">
-                            <br /><br />
 
-                            <textarea name="message" rows="4" cols="50"
+                        <div className="forum-sağ">
+                            <div className="forum">
+                                <div><h1 style={{ color: "hsl(199, 100%, 33%)", textShadow: '1px 1px 0 hsl(200 50% 30%)' }}>SIKÇA SORULAN SORULAR!</h1></div>
+                                <br />
 
-                                className="text-forum"
-                                type='text'
-                                placeholder="Message here..."
 
-                            ></textarea>
-                            <br />
-                            <button className="btn-yorum">SEND</button>
+
+                                Paketleri nasıl satın alabiliriz detaylı bir video var mı ? Teşekkürler
+                                &nbsp;&nbsp;&nbsp;<button className="btn-yorum">KONUYA GİT</button>
+                                <br /><br />
+                                <hr></hr>
+                                <br />
+                                Kendi rotamızı nasıl oluştururuz
+                                &nbsp;&nbsp;&nbsp;<button className="btn-yorum">KONUYA GİT</button>
+                                <br /><br />
+                                <hr></hr>
+                                <br />
+                                Ücret iadesi ne zaman oluyor
+                                &nbsp;&nbsp;&nbsp;<button className="btn-yorum">KONUYA GİT</button>
+                                <br /><br />
+                                <hr></hr>
+
+
+                            </div>
+
 
                         </div>
-                        <br></br>
-
-
-
                     </div>
                 </div>
 
-                <div className="forum-sağ">
-                    <div className="forum">
-                        <div><h1 style={{ color: "hsl(199, 100%, 33%)", textShadow: '1px 1px 0 hsl(200 50% 30%)' }}>!SIKÇA SORULAN SORULAR</h1></div>
-                        <br />
-                        <br></br>
-                        <br></br>
-                        <button className="btn-yorum">KONUYA GİT</button>&nbsp;&nbsp;&nbsp;
-                        Paketleri nasıl satın alabiliriz detaylı bir video var mı ? Teşekkürler
 
-                        <br /><br />
-
-                        <button className="btn-yorum">KONUYA GİT</button>&nbsp;&nbsp;&nbsp;
-                        Kendi rotamızı nasıl oluştururuz
-
-                        <br /><br />
-
-                        <button className="btn-yorum">KONUYA GİT</button>&nbsp;&nbsp;&nbsp;
-                        Ücret iadesi ne zaman oluyor
-
-                        <br /><br />
-
-
-                        <br />
-
-
-                    </div>
-
-
-                </div>
-                <br></br>
 
                 <br /> <br />
 
